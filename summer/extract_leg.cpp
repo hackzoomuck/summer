28eef135be956d3c8de7c9a25e409ed2f807ae1c
leejieun94 <xaq03061@gmail.com>
2018-07-04 10:49:03 +0900

    범위조절

--- summer/extract_leg.cpp
+++ summer/extract_leg.cpp
@@ -1,14 +1,46 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv/highgui.h>
+#include <opencv/cv.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
 
+
+Mat src; Mat src_gray;
+int thresh = 50;
+int max_thresh = 255;
+RNG rng(12345);
+
+void thresh_callback(int, void*)
+{
+	Mat canny_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+
+	/// Detect edges using canny
+	Canny(src_gray, canny_output, thresh, thresh * 2, 3);
+	/// Find contours
+	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	/// Draw contours
+	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+	for (int i = 0; i< contours.size(); i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+	}
+
+	/// Show in a window
+	namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+	imshow("Contours", drawing);
+}
+
 int main(int argc, char** argv)
 {
-
-	
-	Mat inputImg;
+	Mat inputImg, resultImg;
 	Mat hlsImg;
 	Mat skinImg;
 
@@ -50,6 +82,76 @@ int main(int argc, char** argv)
 
 	imshow("Original", inputImg);
 	imshow("SkinDetected", skinImg);
+
+	CV_Assert(skinImg.depth() == CV_8U);
+	resultImg.create(inputImg.size(), inputImg.type());
+
+	const int nChannels = skinImg.channels();
+
+	for (int j = 1; j < skinImg.rows - 1; ++j)
+	{
+		const uchar* prev = skinImg.ptr(j - 1);
+		const uchar* curr = skinImg.ptr(j);
+		const uchar* next = skinImg.ptr(j + 1);
+
+		uchar* output = resultImg.ptr(j);
+		for (int i = nChannels; i < nChannels*(skinImg.cols - 1); ++i)
+		{
+			*output++ = saturate_cast<uchar>(5 * curr[i] - curr[i - nChannels] - curr[i + nChannels] - prev[i] - next[i]);
+		}
+	}
+
+	resultImg.row(0).setTo(Scalar(0));
+	resultImg.row(resultImg.rows - 1).setTo(Scalar(0));
+	resultImg.col(0).setTo(Scalar(0));
+	resultImg.col(resultImg.cols - 1).setTo(Scalar(0));
+
+	/// Load source image and convert it to gray
+	src = resultImg;
+
+	/// Convert image to gray and blur it
+	cvtColor(src, src_gray, CV_BGR2GRAY);
+	blur(src_gray, src_gray, Size(3, 3));
+
+	/// Create Window
+	const char* source_window = "Source";
+	namedWindow(source_window, CV_WINDOW_AUTOSIZE);
+	imshow(source_window, src);
+
+	createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
+	thresh_callback(0, 0);
+	int largest_area = 0;
+	int largest_contour_index = 0;
+	Rect bounding_rect;
+
+	Mat thr(src.rows, src.cols, CV_8UC1);
+	Mat dst(src.rows, src.cols, CV_8UC1, Scalar::all(0));
+	cvtColor(src, thr, CV_BGR2GRAY); //Convert to gray
+	threshold(thr, thr, 25, 255, THRESH_BINARY); //Threshold the gray
+
+	vector<vector<Point>> contours; // Vector for storing contour
+	vector<Vec4i> hierarchy;
+
+	findContours(thr, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // Find the contours in the image
+
+	for (int i = 0; i< contours.size(); i++) // iterate through each contour. 
+	{
+		double a = contourArea(contours[i], false);  //  Find the area of contour
+		if (a>largest_area) {
+			largest_area = a;
+			largest_contour_index = i;                //Store the index of largest contour
+			bounding_rect = boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
+		}
+
+	}
+
+	Scalar color(255, 255, 255);
+	drawContours(dst, contours, largest_contour_index, color, CV_FILLED, 8, hierarchy); // Draw the largest contour using previously stored index.
+	rectangle(src, bounding_rect, Scalar(0, 255, 0), 1, 8, 0);
+	imshow("src", src);
+	imshow("largest Contour", dst);
+	waitKey(0);
+
 
 	waitKey(0);
 	return 0;
